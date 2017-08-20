60272165f3e6913b95e850da1f168f838c5ffa4d
claytonketner <claytonketner@me.com>
2017-08-20 16:06:31 -0700

    Updated resume repo name

--- website/constants.py
+++ website/constants.py
@@ -1,6 +1,6 @@
 RESUME_LINKS = {
-    'resume_dl_link': 'https://github.com/claytonketner/general-resume/raw/'
+    'resume_dl_link': 'https://github.com/claytonketner/resume/raw/'
                       'master/output/Clayton-Ketner_Resume.pdf',
-    'resume_gh_link': 'https://github.com/claytonketner/general-resume/blob/'
+    'resume_gh_link': 'https://github.com/claytonketner/resume/blob/'
                       'master/output/Clayton-Ketner_Resume.pdf',
 }
